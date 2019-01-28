b9eb1c728d5095125e37c52ae63b6edad5889804
gabrielsscavalcante <gabrielsoucavalcante@gmail.com>
2019-01-28 02:53:03 -0300

    Instantiate MainCoordinator for AppDelegate.

--- SimpleCoordinator/AppDelegate.swift
+++ SimpleCoordinator/AppDelegate.swift
@@ -11,11 +11,20 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var coordinator: MainCoordinator?
     var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let navigationController = UINavigationController()
+        coordinator = MainCoordinator(navigationController: navigationController)
+        coordinator?.start()
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
